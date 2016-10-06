32a2da37017c132d792394d87238bdc0c9c29d5b
mapaquin <mapaquin@atomicfiction.com>
2016-10-06 10:11:37 -0400

    darwin ssl fix

--- shotgun_api3/shotgun.py
+++ shotgun_api3/shotgun.py
@@ -67,6 +67,20 @@ LOG.setLevel(logging.WARN)
 SG_TIMEZONE = SgTimezone()
 
 try:
+
+    import platform
+    if platform.system() == 'Darwin':
+        import ssl
+        from functools import wraps
+        def sslwrap(func):
+            @wraps(func)
+            def bar(*args, **kw):
+                kw['ssl_version'] = ssl.PROTOCOL_TLSv1
+                return func(*args, **kw)
+            return bar
+
+        ssl.wrap_socket = sslwrap(ssl.wrap_socket)
+
     import ssl
     NO_SSL_VALIDATION = False
 except ImportError:
